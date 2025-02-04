e5083c1c83a9b089583e20bc4d931405f2c25fab
wekejuice <148364840+wekejuice@users.noreply.github.com>
2025-02-04 22:20:17 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Weke's Bot",
+  botOwner: 'Kevin Weke', // Your name
+  ownerNumber: '+254726961117', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
